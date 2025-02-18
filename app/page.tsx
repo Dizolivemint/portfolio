a549fb651b5df81a862272a91fe870d38194a027
Dizolivemint <somabhi@gmail.com>
2025-02-18 16:48:41 -0700

    🚨 fixed lint error

--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,9 @@ export default async function Page() {
     <div className="max-w-7xl mx-auto p-8">
       <Header />
       <div className="mb-8 text-black dark:text-white text-center">
-        <h1 className='text-xl mb-8'>👋, I'm Miles!</h1>
+        <h1 className='text-xl mb-8'>👋, I&apos;m Miles!</h1>
         <p>
-          Below are some fullstack projects that I've worked on over the years. You can filter them by tech stack. However, much of my work is private. Please <Link href="https://www.linkedin.com/in/milesexner/">visit my LinkedIn profile</Link> for other projects.
+          Below are some fullstack projects that I&apos;ve worked on over the years. You can filter them by tech stack. However, much of my work is private. Please <Link href="https://www.linkedin.com/in/milesexner/">visit my LinkedIn profile</Link> for other projects.
         </p>
       </div>
       <ClientWrapper initialProjects={sortedProjects} />
