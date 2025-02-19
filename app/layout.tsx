14f8e5539a170c9c584ea576d9205a3bfbe7d726
Dizolivemint <somabhi@gmail.com>
2025-02-19 13:58:04 -0700

    💄 added custom favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import '@/app/globals.css';
+import type { Metadata } from "next";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Miles of Code',
   description: 'A showcase of my projects and skills',
+  icons: {
+    icon: [
+      { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
+      { url: '/favicon.svg', type: 'image/svg+xml' },
+      { url: '/favicon.ico' }
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180' }
+    ]
+  },
+  manifest: '/site.webmanifest'
 };
 
 export default function RootLayout({
